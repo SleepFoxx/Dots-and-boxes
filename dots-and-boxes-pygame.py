e157f6473df4406bef9bd53a143d3bf3bc2b7457
Mr.Papa <jakubhulin30@gmail.com>
2022-02-23 17:01:55 +0100

    Uz to fungiruje, zaklad hry hotovy, uvidim ci sa mi bude dnes chciet pokracovat, zomieram

--- dots-and-boxes-pygame.py
+++ dots-and-boxes-pygame.py
@@ -15,6 +15,8 @@ cervena = (255, 0, 0)
 modra = (0, 0, 255)
 zelena = (0, 255, 0)
 cierna = (0, 0, 0)
+
+font = pygame.font.SysFont('cursive', 25)
 #classa samotneho okna
 class miesto:
     def __init__(self, r, c):
@@ -39,14 +41,23 @@ class miesto:
         self.strany = [False, False, False, False]
         self.vyherca = None
 
-    def kontrola_vyhry(self):
+    def kontrola_vyhry(self, vyherca):
         if not self.vyherca:
             if self.strany == [True]*4:
-                self.vyherca = True
+                self.vyherca = vyherca
+                if vyherca == 'X':
+                    self.color = zelena
+                else:
+                    self.color = cervena
+                self.text = font.render(self.vyherca, True, biela)
+
+                return 1
+            return 0
 
     def update(self, okno):
         if self.vyherca:
-            pygame.draw.rect(okno, cervena, self.rect)
+            pygame.draw.rect(okno, self.color, self.rect)
+            okno.blit(self.text, (self.rect.centerx - 5, self.rect.centery - 7))
         for index,  side in enumerate(self.strany):
             if side:
                 pygame.draw.line(okno, biela, (self.okraj[index][0]),
@@ -68,6 +79,15 @@ hore = False
 vpravo = False
 vlavo = False
 dole = False
+
+kolo = 0
+hraci = ['X', 'O']
+hrac = hraci[kolo]
+dalsie_kolo = False
+
+hrac1_skore = 0
+hrac2_skore = 0
+
 #okno hry a priebeh
 bezi = True
 while bezi:
@@ -107,36 +127,82 @@ while bezi:
     for r in range(riadky+1):
         for c in range(stlpce+1):
             pygame.draw.circle(okno, biela, (c*velkost_boxov + 2*padding, r*velkost_boxov + 3*padding), 2)
-    #oznacenie miesta kliku
-    #if pozicia:
-        #pygame.draw.circle(okno, cervena, pozicia, 2)
-
+    
+    #oznacenie stredu aktualne vybrateho stvorca
     for box in boxy:
         box.update(okno)
         if pozicia and box.rect.collidepoint(pozicia):
             bbox = box
     if bbox:  
         index = bbox.index
-        pygame.draw.circle(okno, cervena, (bbox.rect.centerx, bbox.rect.centery), 2)
-            
-        if hore:
+        if not bbox.vyherca:
+            pygame.draw.circle(okno, cervena, (bbox.rect.centerx, bbox.rect.centery), 2)
+        # definicia strany boxov, prepnutie kola po vyplneni strany
+        if hore and not bbox.strany[0]:
             bbox.strany[0] = True
             if index - riadky >= 0:
                 boxy[index - riadky].strany[2] = True
-        if vpravo:
+                dalsie_kolo = True
+        if vpravo and not bbox.strany[1]:
             bbox.strany[1] = True
             if (index + 1) % stlpce > 0:
                 boxy[index + 1].strany[3] = True
-        if dole:
+                dalsie_kolo = True
+        if dole and not bbox.strany[2]:
             bbox.strany[2] = True
             if (index + riadky) < len(boxy):
                 boxy[index + riadky].strany[0] = True
-        if vlavo:
+                dalsie_kolo = True
+        if vlavo and not bbox.strany[3]:
             bbox.strany[3] = True
             if index % stlpce > 0:
                 boxy[index - 1].strany[1] = True
+                dalsie_kolo = True
+        # kontrola vyhri + pocitac kol
+        
+        res = bbox.kontrola_vyhry(hrac)
+        if res:
+            if hrac == 'X':
+                hrac1_skore += 1
+            else:
+                hrac2_skore += 1
 
-        bbox.kontrola_vyhry()
+
+
+        if dalsie_kolo:
+            kolo = (kolo + 1) % len(hraci)
+            hrac = hraci[kolo]
+            dalsie_kolo = False
+    #upresnenie parametrov textu hraca 1
+
+    hrac1foto = font.render(f'Hrac 1: {hrac1_skore}', True, modra)
+    hrac1rect = hrac1foto.get_rect()
+    hrac1rect.x, hrac1rect.y = 2*padding, 15
+
+    
+    #upresnenie parametrov textu hraca 2
+
+    hrac2foto = font.render(f'Hrac 2: {hrac2_skore}', True, modra)
+    hrac2rect = hrac2foto.get_rect()
+    hrac2rect.right, hrac2rect.y = sirka-2*padding, 15
+
+
+    #vypisanie textu hore
+
+    okno.blit(hrac1foto, hrac1rect)
+    okno.blit(hrac2foto, hrac2rect)
+
+
+    #podciarknutie (ukazatel) aktualneho hraca
+
+    if hrac == 'X':
+        pygame.draw.line(okno, modra, (hrac1rect.x, hrac1rect.bottom + 2),
+                            (hrac1rect.right, hrac1rect.bottom + 2), 1)
+    else:
+        pygame.draw.line(okno, modra, (hrac2rect.x, hrac2rect.bottom + 2),
+                            (hrac2rect.right, hrac2rect.bottom + 2), 1)
+
+
     #updatovanie hry aby sme videli vsetky akcie ktore vykoname
     pygame.display.update()
 #vypnutie hry
